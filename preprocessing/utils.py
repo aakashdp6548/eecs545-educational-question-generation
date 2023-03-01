5e32fd4a43909cfc6253af9002c51150ec1bf199
Aakash Patel <aakashdp@umich.edu>
2023-02-28 22:34:11 -0500

    added dataset in hf format

--- preprocessing/utils.py
+++ preprocessing/utils.py
@@ -5,6 +5,7 @@ Various preprocessing util functions.
 import numpy
 import os
 import json
+from datasets import load_dataset
 
 def map_passage2questions(source_filename, target_filename):
     with open(source_filename) as f:
@@ -33,3 +34,13 @@ def save_passage2questions(source_filename, target_filename, output_filename):
     
     with open(output_filename, 'w') as f:
         json.dump(output, f, indent=2)
+
+
+def create_dataset():
+    data_files = {
+        'train': 'test_data/passage-to-questions/p2q-train.json',
+        'dev': 'test_data/passage-to-questions/p2q-dev.json',
+        'test': 'test_data/passage-to-questions/p2q-test.json'
+    }
+    dataset = load_dataset('json', data_files=data_files)
+    return dataset
